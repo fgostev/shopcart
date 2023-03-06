00f49a3d25cb27c2404dd749ef74ab2770f5516b
fgostev <fgostev@gmail.com>
2023-03-06 10:41:03 -0600

    router

---
+++ src/RouteSwitch.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Checkout from "./components/checkout.component";
+import Complete from "./components/complete.component";
+import Item from "./components/item.component";
+import Shop from "./components/shop.component";
+
+// main page
+// catalogue store
+// each item
+// checkout
+// complete
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/shop" element={<Shop />} />
+        {/* make item dynamic */}
+        <Route path="/shop/item" element={<Item />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/complete" element={<Complete />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RouteSwitch;
