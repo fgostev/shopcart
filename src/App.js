1aff5a56222a5243b887ce62f4b2ec0c8fdc5947
fgostev <fgostev@gmail.com>
2023-03-09 11:30:34 -0600

    add assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {useState, useEffect} from 'react';
 
 function App() {
+
+const [items, setItems] = useState([]);
+
+useEffect(() => {
+  fetch(`https://fakestoreapi.com/products/category/men's%20clothing`)
+  .then(response => response.json())
+  .then(data => setItems(data))
+},[])
+
+
   return (
+
     <div className="App">
-    BOOOO!
+    {items.map((item) => <img src = {item.image} alt={item.description} width="200px"></img>)}
     </div>
   );
 }
